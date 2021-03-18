dbb00f71408412b27b336d2316d92fb55d2a8b17
Vinicius Jacik <vinicius-jacik@hotmail.com>
2021-03-18 19:16:36 -0300

    Adicionados Usuarios, Atendentes e Clientes

--- src/main/java/com/uniamerica/aluguelEquipamento/model/Usuarios.java
+++ src/main/java/com/uniamerica/aluguelEquipamento/model/Usuarios.java
@@ -12,6 +12,13 @@ public abstract class Usuarios {
 
     private String nome;
 
+    private String rg;
+
+    private String cpf;
+
+    private String endereco;
+
+    private String telefone;
 
     public long getId() {
         return id;
@@ -28,5 +35,21 @@ public abstract class Usuarios {
     public void setNome(String nome) {
         this.nome = nome;
     }
+
+    public String getRg() { return rg; }
+
+    public void setRg(String rg) { this.rg = rg; }
+
+    public String getCpf() { return cpf; }
+
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getEndereco() { return endereco; }
+
+    public void setEndereco(String endereco) { this.endereco = endereco; }
+
+    public String getTelefone() { return telefone; }
+
+    public void setTelefone(String telefone) { this.telefone = telefone; }
 }
 
