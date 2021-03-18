49384a4451adb42498712fcc85eb91c5491ac4b9
Vinicius Jacik <vinicius-jacik@hotmail.com>
2021-03-17 21:16:19 -0300

    iniciando git

---
+++ src/main/java/com/uniamerica/aluguelEquipamento/AluguelEquipamentoApplication.java
@@ -0,0 +1,14 @@
+package com.uniamerica.aluguelEquipamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelEquipamentoApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AluguelEquipamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uniamerica/aluguelEquipamento/AluguelEquipamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniamerica.aluguelEquipamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluguelEquipamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
