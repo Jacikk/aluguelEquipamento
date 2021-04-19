1b90f0d1ac55cc0ef92e938b914a3d6835a6e409
Vinicius Jacik <vinicius-jacik@hotmail.com>
2021-04-19 16:42:05 -0300

    Adicionado ItensController e tudo que há bom

--- src/main/java/com/uniamerica/aluguelEquipamento/repository/ItensRepository.java
+++ src/main/java/com/uniamerica/aluguelEquipamento/repository/ItensRepository.java
@@ -4,7 +4,12 @@ import com.uniamerica.aluguelEquipamento.model.Itens;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItensRepository extends JpaRepository<Itens, Long> {
 
+    Itens findByNome(String nome);
+
+    List<Itens> findAllByDepartamento(String departamento);
 }
